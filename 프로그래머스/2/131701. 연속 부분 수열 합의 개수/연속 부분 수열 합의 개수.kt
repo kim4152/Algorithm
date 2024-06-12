738447d3ce5a3c9f1fb91ae56b262606197d4d34
kim4152 <108558921+kim4152@users.noreply.github.com>
2024-06-12 18:29:13 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 119.29 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.kt
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.kt
@@ -1,19 +1,20 @@
 class Solution {
     fun solution(elements: IntArray): Int {
+    val check = mutableSetOf<Int>()
     val n = elements.size
-    val uniqueSums = mutableSetOf<Int>()
+    val dp = elements.toMutableList()
 
-    // 모든 길이의 부분 수열에 대해 합을 계산
-    for (length in 1..n) {
-        for (start in 0 until n) {
-            var sum = 0
-            for (i in 0 until length) {
-                sum += elements[(start + i) % n]
-            }
-            uniqueSums.add(sum)
+    check.addAll(dp)
+
+    for (i in 1 until n) {
+        for (j in dp.indices) {
+            dp[j] += elements[(i + j) % n]
         }
+        check.addAll(dp)
     }
 
-    return uniqueSums.size
+    return check.size
 }
+
+
 }
