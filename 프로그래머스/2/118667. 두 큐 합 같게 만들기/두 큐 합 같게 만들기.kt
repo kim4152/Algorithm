1b8164ead2f6ad458ef525dd38f50af2e941ba71
kim4152 <108558921+kim4152@users.noreply.github.com>
2024-06-25 17:17:12 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 65.94 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.kt
@@ -0,0 +1,46 @@
+class Solution {
+    fun solution(queue1: IntArray, queue2: IntArray): Long {
+        var answer: Long = 0
+        
+        var dq1 = ArrayDeque<Long>()
+        var dq2 = ArrayDeque<Long>()
+        
+        queue1.forEach {
+            dq1.add(it.toLong())
+        }
+        
+        queue2.forEach {
+            dq2.add(it.toLong())
+        }
+        
+        var sumOfQueue1: Long = dq1.sum()
+        var sumOfQueue2: Long = dq2.sum()
+        
+        var limitLoopCount = queue1.size * 4
+        
+         while (answer <= limitLoopCount) {
+             if(sumOfQueue1 == sumOfQueue2) {
+                 return answer
+             }
+             
+             ++answer 
+             
+             if(sumOfQueue1 < sumOfQueue2) {
+                 var element = dq2.first()
+                 dq1.add(element)
+                 dq2.removeFirst()
+                 sumOfQueue1 += element
+                 sumOfQueue2 -= element
+                    
+             } else {
+                 var element = dq1.first()
+                 dq2.add(element)
+                 dq1.removeFirst()
+                 sumOfQueue2 += element
+                 sumOfQueue1 -= element
+             }
+        } 
+        
+        return -1
+    }
+}
