551d1e25ce7ae5a4528a449922a0bf9de0c055e5
kim4152 <108558921+kim4152@users.noreply.github.com>
2024-06-27 09:00:39 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.11 ms, Memory: 60.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(n: Int): String {
+        val sb = StringBuilder()
+        repeat(n){it->
+            if(it % 2==0) sb.append("수")
+            else sb.append("박")
+        }
+        return sb.toString()
+    }
+}
