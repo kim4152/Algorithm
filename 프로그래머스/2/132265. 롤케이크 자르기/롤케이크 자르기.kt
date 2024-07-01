62a6d72ccf861c6efa449b071758f06bc1798550
kim4152 <108558921+kim4152@users.noreply.github.com>
2024-07-01 10:07:01 +0900

    [level 2] Title: 롤케이크 자르기, Time: 122.88 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.kt
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.kt
@@ -2,25 +2,21 @@ class Solution {
     fun solution(topping: IntArray): Int {
         var answer = 0
 
-        // 왼쪽과 오른쪽의 토핑 종류 수를 저장할 배열
         val leftCounts = IntArray(topping.size)
         val rightCounts = IntArray(topping.size)
         
-        // 왼쪽에서부터 토핑 종류 수를 세는 세트
         val leftSet = mutableSetOf<Int>()
         for (i in topping.indices) {
             leftSet.add(topping[i])
             leftCounts[i] = leftSet.size
         }
         
-        // 오른쪽에서부터 토핑 종류 수를 세는 세트
         val rightSet = mutableSetOf<Int>()
         for (i in topping.indices.reversed()) {
             rightSet.add(topping[i])
             rightCounts[i] = rightSet.size
         }
         
-        // 두 부분의 토핑 종류 수가 같은 지점을 찾음
         for (i in 0 until topping.size - 1) {
             if (leftCounts[i] == rightCounts[i + 1]) {
                 answer++
