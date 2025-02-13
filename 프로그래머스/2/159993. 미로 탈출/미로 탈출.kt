50113c955a8047afcc717dafb0fe78975b8c4454
kim4152 <108558921+kim4152@users.noreply.github.com>
2025-02-13 10:29:53 +0900

    [level 2] Title: 미로 탈출, Time: 40.67 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.kt
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.kt
@@ -2,27 +2,28 @@ import java.util.*
 
 class Solution {
     data class Node(val x: Int, val y: Int, val count: Int, val isL: Boolean)
-
     fun solution(maps: Array<String>): Int {
-        val map = maps.toList()
-        val start = findStart(maps) ?: return -1
-        val queue: Queue<Node> = LinkedList()
-        val visited = Array(map.size) { Array(map[0].length) { BooleanArray(2) } } // [x][y][0] = L 미통과, [x][y][1] = L 통과
+        val (startX, startY) = findStart(maps) ?: return -1
+        val answer = bfs(Node(startX, startY, 0, false), maps)
+        return answer
+    }
 
-        queue.offer(Node(start.first, start.second, 0, false))
-        visited[start.first][start.second][0] = true
+    fun bfs( node: Node,  maps: Array<String>): Int{
+        val queue : Queue<Node> = LinkedList()
+        val visited = Array(maps.size){Array(maps[0].length){BooleanArray(2)}}
+        
+        queue.offer(node)
 
-        while (queue.isNotEmpty()) {
+        while(queue.isNotEmpty()){
             val (x, y, count, isL) = queue.poll()
-
-            if (isL && map[x][y] == 'E') return count // L을 지나고 도착점 도달하면 반환
-
-            for ((dx, dy) in listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)) {
+            if(isL && maps[x][y] == 'E') return count
+            
+            for( (dx, dy) in listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1) ){
                 val nx = x + dx
                 val ny = y + dy
-
-                if (nx in map.indices && ny in map[0].indices && map[nx][ny] != 'X') {
-                    val nextL = isL || map[nx][ny] == 'L'
+                
+                if (nx in maps.indices && ny in maps[0].indices && maps[nx][ny] != 'X') {
+                    val nextL = isL || maps[nx][ny] == 'L'
                     val lIndex = if (nextL) 1 else 0
                     if (!visited[nx][ny][lIndex]) {
                         visited[nx][ny][lIndex] = true
@@ -31,13 +32,15 @@ class Solution {
                 }
             }
         }
-        return -1 // 도착 불가능
+        return -1
     }
 
-    fun findStart(maps: Array<String>): Pair<Int, Int>? {
-        for (x in maps.indices) {
-            val y = maps[x].indexOf('S')
-            if (y != -1) return x to y
+
+    fun findStart(maps: Array<String>): Pair<Int,Int>?{
+        maps.forEachIndexed{ x, s ->
+            s.forEachIndexed{ y, c ->
+                if(c == 'S') return Pair(x, y)
+            }
         }
         return null
     }
